484285317e0bc9cddfcbe3709ac0290345e03ae1
Tom <121997330+TomCatWithSuger@users.noreply.github.com>
2026-08-06 15:54:13 +0800

    Revert "feat: 添加经典生成模型"

--- tests/test_datamodules.py
+++ tests/test_datamodules.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import pytest
 import torch
 
-from src.data.atcosim_datamodule import ATCOSIMDataModule
 from src.data.mnist_datamodule import MNISTDataModule
 
 
@@ -37,31 +36,3 @@ def test_mnist_datamodule(batch_size: int) -> None:
     assert len(y) == batch_size
     assert x.dtype == torch.float32
     assert y.dtype == torch.int64
-
-
-@pytest.mark.parametrize("batch_size", [2, 4])
-def test_atcosim_datamodule(batch_size: int) -> None:
-    data_dir = "/media/B8BE-8B5F/Tom/data/ATCOSIM"
-
-    dm = ATCOSIMDataModule(data_dir=data_dir, batch_size=batch_size)
-
-    dm.prepare_data()
-    dm.setup()
-
-    assert dm.data_train is not None
-    assert dm.data_val is not None
-    assert dm.data_test is not None
-    assert len(dm.data_train) == 5_745
-    assert len(dm.data_val) == 1_436
-    assert len(dm.data_test) == 1_760
-
-    batch = next(iter(dm.train_dataloader()))
-    assert batch["waveforms"].shape[0] == batch_size
-    assert batch["waveforms"].dtype == torch.float32
-    assert batch["lengths"].shape == (batch_size,)
-    assert batch["lengths"].dtype == torch.int64
-    assert batch["attention_mask"].shape == batch["waveforms"].shape
-    assert batch["attention_mask"].dtype == torch.bool
-    assert batch["sample_rate"] == 32_000
-    assert len(batch["ids"]) == batch_size
-    assert len(batch["texts"]) == batch_size
